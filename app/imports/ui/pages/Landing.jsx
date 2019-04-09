00c4bab153595185664c242a426f7153060d4558
kobyv <Kobyv@hawaii.edu>
2019-04-09 10:09:02 -1000

    worked on category page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,24 +1,41 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Header, Dropdown, Menu } from 'semantic-ui-react';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
+import { withRouter, NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
+    const menuStyle = { marginBottom: '10px' };
     return (
         <Grid verticalAlign='middle' textAlign='center' container>
-
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
-
           <Grid.Column width={8}>
             <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
+            <Menu style={menuStyle} attached="top" borderless>
 
+            {this.props.currentUser ? (
+                [<Menu.Item as={NavLink} activeClassName="active" exact to="/text" key='text'>Textbooks</Menu.Item>,
+                  <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>List Stuff</Menu.Item>]
+            ) : ''}
+            </Menu>
+          </Grid.Column>
         </Grid>
     );
   }
 }
 
-export default Landing;
+
+Landing.propTypes = {
+  currentUser: PropTypes.string,
+};
+
+const LandingContainer = withTracker(() => ({
+  currentUser: Meteor.user() ? Meteor.user().username : '',
+}))(Landing);
+
+export default withRouter(LandingContainer);
+
+
