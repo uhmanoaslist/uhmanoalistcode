b6abf7c914ff4a69e0fbca112311e82a30197ee1
Hoyeon Jang <46728564+qkeb700@users.noreply.github.com>
2019-04-10 20:56:36 -1000

    finished landing

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
+        <div className='uhmanoaslist-landing-page'>
+          <Grid container stackable centered columns={1}>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
-
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
-
-        </Grid>
+            <Grid.Column textAlign='center'>
+              <h1 className='header' inverted>Welcome to UHManoasList</h1>
+              <h3 className='header2' inverted>Feel free to buy and sell student related goods and services!</h3>
+            </Grid.Column>
+          </Grid>
+        </div>
     );
   }
 }
