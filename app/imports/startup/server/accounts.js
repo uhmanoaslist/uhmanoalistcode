b1e5bcb215b7598b1cd0b5ca0e66efe427db04a4
markm24 <markmatsuoka.24@gmail.com>
2019-04-11 12:28:09 -1000

    Created "Profiles" collection along with schema and default

--- app/imports/startup/server/accounts.js
+++ app/imports/startup/server/accounts.js
@@ -4,15 +4,12 @@ import { Roles } from 'meteor/alanning:roles';
 
 /* eslint-disable no-console */
 
-function createUser(email, password, phone, picture, bio, role) {
+function createUser(email, password, role) {
   console.log(`  Creating user ${email}.`);
   const userID = Accounts.createUser({
     username: email,
     email: email,
     password: password,
-    phone: phone,
-    picture: picture,
-    bio: bio,
   });
   if (role === 'admin') {
     Roles.addUsersToRoles(userID, 'admin');
@@ -23,7 +20,7 @@ function createUser(email, password, phone, picture, bio, role) {
 if (Meteor.users.find().count() === 0) {
   if (Meteor.settings.defaultAccounts) {
     console.log('Creating the default user(s)');
-    Meteor.settings.defaultAccounts.map(({ email, password, phone, picture, bio, role }) => createUser(email, password, phone, picture, bio, role));
+    Meteor.settings.defaultAccounts.map(({ email, password, role }) => createUser(email, password, role));
   } else {
     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
---
+++ app/imports/startup/server/profile.js
@@ -0,0 +1,34 @@
+import { Meteor } from 'meteor/meteor';
+import { Roles } from 'meteor/alanning:roles';
+import { Profiles } from '../../api/profile/profile.js';
+
+/** Initialize the database with a default data document. */
+function addData(data) {
+  console.log(`  Adding: ${data.username}`);
+  Profiles.insert(data);
+}
+
+/** Initialize the collection if empty. */
+if (Profiles.find().count() === 0) {
+  if (Meteor.settings.defaultProfiles) {
+    console.log('Creating default profiles.');
+    Meteor.settings.defaultProfiles.map(data => addData(data));
+  }
+}
+
+/** This subscription publishes only the documents associated with the logged in user */
+Meteor.publish('Profiles', function publish() {
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userId).username;
+    return Profiles.find({ email: username });
+  }
+  return this.ready();
+});
+
+/** This subscription publishes all documents regardless of user, but only if the logged in user is the Admin. */
+Meteor.publish('ProfileAdmin', function publish() {
+  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
+    return Profiles.find();
+  }
+  return this.ready();
+});
